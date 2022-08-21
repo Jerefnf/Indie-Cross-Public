fc9c8ed8c6ebd1dfa269b0e4b01d694da1baf618
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-08-21 14:07:15 +0300

    Update DiamondTransSubState.hx

--- source/DiamondTransSubState.hx
+++ source/DiamondTransSubState.hx
@@ -19,7 +19,7 @@ class DiamondTransSubState extends FlxSubState
 
 	var fi:Bool = true;
 
-	public function new(duration:Float = 1, fadeIn:Bool = true, finishCallback:() -> Void = null)
+	public function new(duration:Float = 1.0, fadeIn:Bool = true, finishCallback:() -> Void = null)
 	{
 		super();
 
