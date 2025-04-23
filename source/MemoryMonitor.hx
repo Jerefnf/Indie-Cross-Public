1bb1f7781bbbdf37e76a2704d5f46dbc9dce4cde
Jerefnf <101482201+Jerefnf@users.noreply.github.com>
2025-04-23 13:05:05 -0500

    krico

--- source/MemoryMonitor.hx
+++ source/MemoryMonitor.hx
@@ -4,62 +4,113 @@ import cpp.vm.Gc;
 import openfl.events.Event;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
+import openfl.filters.GlowFilter;
+import haxe.Timer;
 
-// https://imgur.com/a/LVkQmqe
 #if windows
 @:headerCode("
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <psapi.h>
-// are you serious?? 
-// do i have to include this after windows.h to not get outrageous compilation errors??????
-// one side of my brains loves c++ and the other one hates it
 ")
 #end
+
 class MemoryMonitor extends TextField
 {
-	private var times:Array<Float>;
 	private var memPeak:Float = 0;
+	private var lastMem:Float = 0;
+	private var dangerTime:Float = 0;
+	private var hue:Float = 0;
 
-	public function new(inX:Float = 10.0, inY:Float = 10.0, inCol:Int = 0x000000)
+	public function new(inX:Float = 10.0, inY:Float = 10.0)
 	{
 		super();
-
 		x = inX;
 		y = inY;
+		width = 180;
+		height = 60;
 		selectable = false;
-		defaultTextFormat = new TextFormat("_sans", 12, inCol);
-
+		multiline = true;
+		background = true;
+		backgroundColor = 0x000000;
+		border = true;
+		defaultTextFormat = new TextFormat("_typewriter", 13, 0x00FF99);
+		text = "[ INIT ]";
 		addEventListener(Event.ENTER_FRAME, onEnter);
-		width = 150;
-		height = 70;
 	}
 
 	private function onEnter(_)
 	{
-		#if windows
-		// now be an ACTUAL real man and get the memory from plain & straight c++
-		var actualMem:Float = obtainMemory();
-		#else
-		// be a real man and calculate memory from hxcpp
-		var actualMem:Float = Gc.memInfo64(3); // update: this sucks
-		#end
-		var mem:Float = Math.round(actualMem / 1024 / 1024 * 100) / 100;
-		if (mem > memPeak)
-			memPeak = mem;
+		var now = Timer.stamp();
+		var mem = getMemoryInMB();
+		var diff = mem - lastMem;
+		lastMem = mem;
 
-		if (visible)
-		{
-			text = "\nMEM: " + mem + " MB\nMEM peak: " + memPeak + " MB";
+		if (mem > memPeak) memPeak = mem;
+		if (diff > 100) dangerTime = now + 3;
+
+		if (now < dangerTime) {
+			textColor = 0xFF0000;
+			borderColor = 0xFF0000;
+			filters = [new GlowFilter(0xFF0000, 1, 6, 6, 2)];
+		} else {
+			hue += 1;
+			if (hue >= 360) hue = 0;
+			var rgb = hslToRgb(hue / 360, 1, 0.5);
+			var color = (rgb.r << 16) | (rgb.g << 8) | rgb.b;
+			textColor = color;
+			borderColor = color;
+			filters = [new GlowFilter(color, 1, 6, 6, 2)];
 		}
+
+		text = "[ MEM:  " + format(mem) + " MB ]\n[ PEAK: " + format(memPeak) + " MB ]";
 	}
 
-	#if windows // planning to do the same for linux but im lazy af so rn it'll use the hxcpp gc
+	private inline function format(v:Float):String
+	{
+		return Std.string(Math.round(v * 100) / 100);
+	}
+
+	private function getMemoryInMB():Float
+	{
+		#if windows
+		return obtainMemory() / 1024 / 1024;
+		#else
+		return Gc.memInfo64(3) / 1024 / 1024;
+		#end
+	}
+
+	private function hslToRgb(h:Float, s:Float, l:Float):{r:Int, g:Int, b:Int}
+	{
+		var r:Float, g:Float, b:Float;
+		if (s == 0) {
+			r = g = b = l;
+		} else {
+			function hue2rgb(p:Float, q:Float, t:Float):Float {
+				if (t < 0) t += 1;
+				if (t > 1) t -= 1;
+				if (t < 1 / 6) return p + (q - p) * 6 * t;
+				if (t < 1 / 2) return q;
+				if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+				return p;
+			}
+			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			var p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1 / 3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1 / 3);
+		}
+		return {
+			r: Std.int(r * 255),
+			g: Std.int(g * 255),
+			b: Std.int(b * 255)
+		};
+	}
+
+	#if windows
 	@:functionCode("
-		// ily windows api <3
 		auto memhandle = GetCurrentProcess();
 		PROCESS_MEMORY_COUNTERS pmc;
-
 		if (GetProcessMemoryInfo(memhandle, &pmc, sizeof(pmc)))
 			return(pmc.WorkingSetSize);
 		else
